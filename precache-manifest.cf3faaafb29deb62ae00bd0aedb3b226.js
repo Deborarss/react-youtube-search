ecac6447027638e1ec720cd67196f7fd5cf03366
Debora <debora_rss@hotmail.com>
2019-05-14 14:19:05 -0300

    Updates

---
+++ precache-manifest.cf3faaafb29deb62ae00bd0aedb3b226.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d750795eba1de1b2a659a5532db96d",
+    "url": "/react-youtube-search/index.html"
+  },
+  {
+    "revision": "2cc6c811cb02a3e05abb",
+    "url": "/react-youtube-search/static/css/main.35141037.chunk.css"
+  },
+  {
+    "revision": "7e6ac1444ef9242ebc00",
+    "url": "/react-youtube-search/static/js/2.824d5390.chunk.js"
+  },
+  {
+    "revision": "2cc6c811cb02a3e05abb",
+    "url": "/react-youtube-search/static/js/main.13a7a8ad.chunk.js"
+  },
+  {
+    "revision": "6ee5bad95b0f64b5c572",
+    "url": "/react-youtube-search/static/js/runtime~main.c50d4f57.js"
+  }
+]);
